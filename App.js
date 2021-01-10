146aa6f2441f692650168a9243345826a5229990
Robert Gabriel <anyrobert@outlook.com.br>
2021-01-09 23:49:39 -0300

    run camera on webview

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
+import * as Permissions from "expo-permissions";
+import { usePermissions } from "expo-permissions";
 export default function App() {
+  const [cameraPermission, askCameraPermission] = usePermissions(
+    Permissions.CAMERA
+  );
+  const [micPermission, askMicPermission] = usePermissions(
+    Permissions.AUDIO_RECORDING
+  );
+  const [mediaPermission, askMediaPermission] = usePermissions(
+    Permissions.MEDIA_LIBRARY
+  );
+  useEffect(() => {
+    if (cameraPermission && !cameraPermission.status !== "granted") {
+      askCameraPermission();
+    }
+    if (mediaPermission && !mediaPermission.status !== "granted") {
+      askMediaPermission();
+    }
+    if (micPermission && !micPermission.status !== "granted") {
+      askMicPermission();
+    }
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      userAgent={'Browser'}
+      mediaPlaybackRequiresUserAction={false}
+      source={{ uri: "https://36a9661ce8e9.ngrok.io" }}
+      style={{ marginTop: 20 }}
+      geolocationEnabled={true}
+      mediaPlaybackRequiresUserAction={false}
+      useWebKit
+      originWhitelist={["*"]}
+      allowsInlineMediaPlayback
+      javaScriptEnabled={true}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
